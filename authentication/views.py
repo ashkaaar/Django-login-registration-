7c125821aa675c957b7f0cecaa5d44ca2d70bef6
av1shkar <vishkardandge@gmail.com>
2024-01-16 22:31:18 +0530

    auth app configurations

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            login(request, user)
+            # Implement email confirmation logic here
+            return redirect('home')  # Redirect to your home page
+    else:
+        form = RegistrationForm()
+    return render(request, 'authentication/register.html', {'form': form})
