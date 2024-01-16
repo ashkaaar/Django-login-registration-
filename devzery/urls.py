d2c2417c15fd5661bb57a0080d7c04afe16de871
Avishkar <vishkardandge@gmail.com>
2024-01-16 23:05:46 +0530

    Home removed

--- devzery/urls.py
+++ devzery/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
-    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
