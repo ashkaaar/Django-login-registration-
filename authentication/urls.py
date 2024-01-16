0de18c1b611b13e424e91393f3b7d1a378ee8b10
Avishkar <vishkardandge@gmail.com>
2024-01-16 23:00:37 +0530

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import register
-
+from django.urls import path, include
+from . import views
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('', views.home, name = 'home'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('signup',views.signup, name='signup'),
+    path('signin',views.signin, name='signin'),
+    path('signout',views.signout, name='signout'),
 ]
